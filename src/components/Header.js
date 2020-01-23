73caa8a844185cf43bfb44f1f08a0fa0d645a52a
ankit_yadav1 <ankit_yadav1@persistent.com>
2020-01-23 20:10:05 +0530

    Header in progress

---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+<div className="container">        
+    <div className="Header">
+      <div className="primary">
+          <ul>
+              <li><a href="#">CREDITUNION</a></li>
+              <li><a href="#">INSURANCE</a></li>
+              <li><a href="#">REALTY</a></li>
+              <li><a href="#">INVESTMENT</a></li>
+          </ul>
+      </div>
+      <div className="secondary">
+          <ul>
+              <li><a href="#">Community</a></li>
+              <li><a href="#">Branches&ATMs</a></li>
+              <li><a href="#">Careers</a></li>
+          </ul>
+      </div>
+      <div className="tertiary">
+          <ul>
+            <li><a href="#">LOGIN</a></li>
+            <li><a href="#">SEARCH </a></li>
+          </ul>
+
+      </div>
+    </div>
+    </div>
+    );
+  }
+}
+
+export default Header;
