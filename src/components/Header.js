108c8d9dac670309e2a76a4d90c67aadd1972dcc
ankit_yadav1 <ankit_yadav1@persistent.com>
2020-01-27 21:04:07 +0530

    Header flex box applied

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,34 +3,37 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-<div className="container">        
-    <div className="Header">
-      <div className="primary">
-          <ul>
-              <li><a href="#">CREDIT UNION</a></li>
-              <li><a href="#">INSURANCE</a></li>
-              <li><a href="#">REALTY</a></li>
-              <li><a href="#">INVESTMENT</a></li>
-          </ul>
-      </div>
-      <div className="secondary">
-          <ul>
-              <li><a href="#">Community</a></li>
-              <span>|</span>
-              <li><a href="#">Branches & ATMs</a></li>
-              <li><a href="#">Careers</a></li>
-          </ul>
-      </div>
-      <div className="tertiary">
-          <ul>
-            <li ><a href="#">LOGIN</a></li>
-            <span>|</span>
-            <li><a href="#">SEARCH</a></li>
-          </ul>
-
-      </div>
-    </div>
-    </div>
+        <header>
+            <nav>
+                <div className="container">        
+                    <div className="TopNavbar">
+                        <div className="primary">
+                        <ul>
+                            <li><a href="#">CREDIT UNION</a></li>
+                            <li><a href="#">INSURANCE</a></li>
+                            <li><a href="#">REALTY</a></li>
+                            <li><a href="#">INVESTMENT</a></li>
+                        </ul>
+                        </div>
+                        <div className="secondary">
+                        <ul>
+                            <li><a href="#">Community</a></li>
+                            <span>|</span>
+                            <li><a href="#">Branches & ATMs</a></li>
+                            <li><a href="#">Careers</a></li>
+                        </ul>
+                        </div>
+                        <div className="tertiary">
+                        <ul>
+                            <li ><a href="#">LOGIN</a></li>
+                            <span>|</span>
+                            <li><a href="#">SEARCH</a></li>
+                        </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
     );
   }
 }
