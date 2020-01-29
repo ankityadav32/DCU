143438ba05a59e3ff894a46d8029d8b75b157347
ankit_yadav1 <ankit_yadav1@persistent.com>
2020-01-29 22:09:47 +0530

    Header responsive completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 class Header extends Component {
   render() {
     return (
@@ -9,7 +8,7 @@ class Header extends Component {
                     <div className="TopNavbar">
                         <div className="primary">
                         <ul>
-                            <li><a href="#">CREDIT UNION</a></li>
+                            <li><a id="Special" href="#">CREDIT UNION</a></li>
                             <li><a href="#">INSURANCE</a></li>
                             <li><a href="#">REALTY</a></li>
                             <li><a href="#">INVESTMENT</a></li>
@@ -17,22 +16,27 @@ class Header extends Component {
                         </div>
                         <div className="secondary">
                         <ul>
-                            <li><a href="#">Community</a></li>
+                            <li><a  id="Special" href="#">Community</a></li>
                             <span>|</span>
                             <li><a href="#">Branches & ATMs</a></li>
                             <li><a href="#">Careers</a></li>
                         </ul>
                         </div>
                         <div className="tertiary">
-                        <ul>
+                        <ul >
+                            <span><i  className="far fa-user"></i></span>
                             <li ><a href="#">LOGIN</a></li>
-                            <span>|</span><span><i class="fas fa-search"></i></span>
+                            <span>|</span>
+                            <span><i className="fas fa-search"></i></span>
                             <li><a href="#">SEARCH</a></li>
                         </ul>
                         </div>
                     </div>
                     <div className="BottomNavbar">
+                        <span><i id="menubars" className="fas fa-bars"></i></span>
                         <img src="DCUGreen.svg" alt="DCU logo"/>
+                        <span><i id="logo" className="fas fa-search"></i></span>
+                        <span><i id="logo" className="far fa-user"></i></span>
                         <ul>
                             <li><a href="#">Bank</a></li>
                             <li><a href="#">Borrow</a></li>
@@ -41,6 +45,7 @@ class Header extends Component {
                             <li><a href="#">Access</a></li>
                             <li><a href="#">Services</a></li>
                             <li><a href="#">Join</a></li>
+                            
                         </ul>
                     </div>
                 </div>
