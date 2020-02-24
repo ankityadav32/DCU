106acae10cef4531f2c84afe050e01eac4ebd333
ankit_yadav1 <ankit_yadav1@persistent.com>
2020-02-24 22:00:13 +0530

    header modified

--- src/components/tester.js
+++ src/components/tester.js
@@ -5,40 +5,41 @@ class Tester extends Component {
         return (
             <div className="outer_container">
                 <div className="inner_container">
-                    <nav>
-                        <ul id="primary-nav">
+                    <nav className="primary_nav">
+                        <ul>
                             <li><a id="Special" href="#">CREDIT UNION</a></li>
                             <li><a href="#" >INSURANCE</a></li>
                             <li><a href="#" >REALTY</a></li>
                             <li><a href="#" >INVESTMENT</a></li>
                         </ul>
                     </nav>
-                    <div className="navbar2">
-                        <nav id="outer_middle-nav">
-                            <ul id="middle-nav">
-                                <li><a id="Special" href="#">Community</a></li>
-                                <span>|</span>
-                                <li><a href="#">Branches & ATMs</a></li>
-                                <li><a href="#">Careers</a></li>
+                    <nav className="secondary_nav" >
+                        <ul>
+                            <li><a id="Special" href="#">Community</a></li>
+                            <span>|</span>
+                            <li><a href="#">Branches & ATMs</a></li>
+                            <li><a href="#">Careers</a></li>
 
-                            </ul>
-                        </nav>
-                        <nav className="bsdk">
-                            <div id="triangle"></div>
-                            <ul id="tertiary-nav">
+                        </ul>
+                    </nav>
+                    <nav className="tertiary_nav">
+                        <ul>
 
-                                <span><i className="far fa-user"></i></span>
-                                <li ><a href="#">LOGIN</a></li>
-                                <span>|</span>
-                                <span><i className="fas fa-search"></i></span>
-                                <li><a href="#">SEARCH</a></li>
-                            </ul>
-                        </nav>
-                    </div>
+                            <li ><a href="#">LOGIN</a></li>
+
+
+                            <li><a href="#">SEARCH</a></li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
+
         );
     }
 }
 
 export default Tester;
+
+{/* <span><i className="far fa-user"></i></span>
+<span><i className="fas fa-search"></i></span>
+<span>|</span> */}
